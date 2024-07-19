f0e28495ff7ffd2c30689e4a42964beff6d8f416
[Ramraj Salunkhe] <[ramrajsalunkhe11@gmail.com]>
2024-07-19 23:20:32 +0530

    first Commit

--- Main.java
+++ Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("This is java Program");
         System.out.println("output from java Program");
         System.out.println(new Date());
+        System.out.println("Hii Ramraj Salunkhe neew comit it is");
+        System.out.println(new Date());
     }
 }
